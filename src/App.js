b71aa033ef012df9d736fe0a4dbfe65a269f5481
Julia Melnik <yuliyamielnik@gmail.com>
2020-05-25 20:27:33 +0200

    add opacity

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import React from 'react';
-import {connect} from 'react-redux';
-import {BrowserRouter, Switch, Route} from 'react-router-dom';
+import { connect } from 'react-redux';
+import { BrowserRouter, Route } from 'react-router-dom';
 import PropTypes from 'prop-types';
+import { AnimatedSwitch } from 'react-router-transition';
 
 import MainLayout from './components/layout/MainLayout/MainLayout';
 
@@ -15,7 +16,8 @@ import Info from './components/views/Info/Info';
 import NotFound from './components/views/NotFound/NotFound';
 
 import parseTrips from './utils/parseTrips';
-import {setMultipleStates} from './redux/globalRedux';
+import { setMultipleStates } from './redux/globalRedux';
+import styles from './App.scss';
 
 class App extends React.Component {
   static propTypes = {
@@ -23,24 +25,30 @@ class App extends React.Component {
     setStates: PropTypes.func,
   }
 
-  constructor(props){
+  constructor(props) {
     super(props);
     // parse trips when App is first created
     parseTrips(this.props.trips, this.props.setStates);
   }
 
-  componentDidUpdate(prevProps){
-    if(prevProps.trips != this.props.trips){
+  componentDidUpdate(prevProps) {
+    if (prevProps.trips != this.props.trips) {
       // parse trips again if they changed
       parseTrips(this.props.trips, this.props.setStates);
     }
   }
 
-  render(){
+  render() {
     return (
       <BrowserRouter>
         <MainLayout>
-          <Switch location={location}>
+          <AnimatedSwitch
+            atEnter={{ opacity: 0, marginTop: 200 }}
+            atLeave={{ opacity: 0, marginTop: -200 }}
+            atActive={{ opacity: 1, marginTop: 0 }}
+            className={styles.switchWrapper}
+            location={location}
+          >
             <Route exact path='/' component={Home} />
             <Route exact path='/trips' component={Trips} />
             <Route exact path='/trip/:id' component={Trip} />
@@ -49,7 +57,7 @@ class App extends React.Component {
             <Route exact path='/regions' component={Regions} />
             <Route exact path='/info' component={Info} />
             <Route path='*' component={NotFound} />
-          </Switch>
+          </AnimatedSwitch>
         </MainLayout>
       </BrowserRouter>
     );
