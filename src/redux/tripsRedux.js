489f1aa22a29cd3bcda73eb8bfff9b822b090b99
Julia Melnik <yuliyamielnik@gmail.com>
2020-05-25 19:40:50 +0200

    fix countries & regions

--- src/redux/tripsRedux.js
+++ src/redux/tripsRedux.js
@@ -13,24 +13,32 @@ export const getFilteredTrips = ({trips, filters}) => {
 
   // TODO - filter by duration
 
+  if(filters.searchPhrase){
+    const pattern = new RegExp(filters.searchPhrase, 'i');
+    output = output.filter(trip => pattern.test(trip.name));
+  }
   // TODO - filter by tags
-
+  if(filters.tags){
+    const pattern = new RegExp(filters.tags, 'i');
+    output = output.filter(trip => pattern.test(trip.tags));
+  }
   // TODO - sort by cost descending (most expensive goes first)
+  
 
   return output;
 };
 
-export const getTripById = ({trips}, tripId) => {
-  const filtered = trips;
-
+export const getTripById = ({ trips }, tripId) => {
+  const filtered = trips.filter(trip => trip.id == tripId);
+  
   // TODO - filter trips by tripId
 
   console.log('filtering trips by tripId:', tripId, filtered);
-  return filtered.length ? filtered[0] : {error: true};
+  return filtered.length ? filtered[0] : { error: true };
 };
 
-export const getTripsForCountry = ({trips}, countryCode) => {
-  const filtered = trips;
+export const getTripsForCountry = ({ trips }, countryCode) => {
+  const filtered = trips.filter(trip => trip.country.code == countryCode);
 
   // TODO - filter trips by countryCode
 
